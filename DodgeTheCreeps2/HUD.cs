e8703ddea23a7359ef854585c2fe42c51770d6e7
horatius83 <horatius83@gmail.com>
2020-11-09 16:53:32 -0600

    Finished work on HUD

--- DodgeTheCreeps2/HUD.cs
+++ DodgeTheCreeps2/HUD.cs
@@ -29,4 +29,20 @@ public class HUD : CanvasLayer
         await ToSignal(GetTree().CreateTimer(1), "timeout");
         GetNode<Button>("StartButton").Show();
     }
+
+    public void UpdateScore(int score)
+    {
+        GetNode<Label>("ScoreLabel").Text = score.ToString();
+    }
+
+    public void OnMessageTimerTimeout()
+    {
+        GetNode<Label>("Message").Hide();
+    }
+
+    public void OnStartButtonPressed()
+    {
+        GetNode<Button>("StartButton").Hide();
+        EmitSignal("StartGame");
+    }
 }
